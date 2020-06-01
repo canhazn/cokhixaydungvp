10eb377cd106f8dd7cd658e47be2428bbeda2426
tuan anh <canhazn@gmail.com>
2020-06-01 19:12:07 +0700

    ok

--- cokhixaydungvp/settings.py
+++ cokhixaydungvp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8t@d*%o%!ynf3$y+e#etf(-gxof8!rc%p3vtgf9)#4hh#hl+o7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
